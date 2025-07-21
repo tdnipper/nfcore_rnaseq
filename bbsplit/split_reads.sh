f18fdd3c2666a7e770bb1f8c0b36cb44ec84de64
Thomas <nipper@wisc.edu>
2025-07-21 15:25:26 +0000

    change to run container with bbsplit

--- bbsplit/split_reads.sh
+++ bbsplit/split_reads.sh
@@ -1,14 +1,5 @@
-#!/bin/bash
-
-RAW_DATA_DIR="/raw_data/"
-
-OUTPUT_DIR="/out/"
-
-cd "$(dirname "$0")"
-
-for reads_file in "$RAW_DATA_DIR"/*_human.fastq.gz; do
-    BASENAME=$(basename "$reads_file" _human.fastq.gz)
-    reformat.sh in="$reads_file" \
-        out1="$OUTPUT_DIR/${BASENAME}_R1.fastq.gz" \
-        out2="$OUTPUT_DIR/${BASENAME}_R2.fastq.gz"
-done
+podman run --rm -it \
+    -v ./genomes/:/genomes/ \
+    -v ./bbsplit/:/out/ \
+    -v ./raw_data/:/raw_data/ \
+    bbmap:39.19--he5f24ec_0 bash /out/run_bbsplit.sh
