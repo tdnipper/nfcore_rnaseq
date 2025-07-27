900d2b7089146659f680a66371e23d4e53b8989e
Thomas <nipper@wisc.edu>
2025-07-27 16:09:08 +0000

    rename/reorganize files

--- bbsplit/create_index.sh
+++
@@ -1,1 +0,0 @@
-podman run --rm -it -v ./genomes/:/genomes/ -v ./bbsplit/:/bbsplit/ bbmap:39.19--he5f24ec_0 bbsplit.sh ref_human=/genomes/GRCh38.primary_assembly.genome.fa.gz ref_flu=/genomes/wsn_1933.fasta.gz path=/bbsplit/
