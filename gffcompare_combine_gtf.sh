2299e329c1bae8d13d9c412d30cc42bbc52883a0
Thomas <nipper@wisc.edu>
2025-07-31 15:15:14 +0000

    fix: update gffcompare_combine_gtf.sh to use correct GTF files and output directory

--- gffcompare_combine_gtf.sh
+++ gffcompare_combine_gtf.sh
@@ -4,9 +4,8 @@ podman run --rm -it \
     -v ./results_hisat2/:/results_hisat2/ \
     quay.io/biocontainers/gffcompare:0.12.6--h9948957_4 \
     bash -c " 
-        cp /results_hisat2/genome/hybrid_gencode_fixed.filtered.gtf hybrid_gencode_fixed.gtf && \
-        gunzip -c /stringtie/stringtie_transcriptome.filtered.gtf.gz -c > stringtie_transcriptome.filtered.gtf && \
-        gffcompare -V -r hybrid_gencode_fixed.gtf \
-        -o /stringtie/upstream_merge \
-        /results_hisat2/hisat2/stringtie/*.gtf
+        cp /results_hisat2/genome/hybrid_gencode.filtered.gtf hybrid_gencode.filtered.gtf && \
+        gffcompare -V -r hybrid_gencode.filtered.gtf \
+        -o /stringtie/compare \
+        /stringtie/stringtie_merged_transcriptome.gtf 
     "
