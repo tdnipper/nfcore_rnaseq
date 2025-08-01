e4a67db42cfddc6d3e796eb6cc51699a777ad829
Thomas <nipper@wisc.edu>
2025-08-01 18:39:01 +0000

    fix: update gffcompare_combine_gtf.sh to use fixed GTF file names

--- gffcompare_combine_gtf.sh
+++ gffcompare_combine_gtf.sh
@@ -4,8 +4,8 @@ podman run --rm -it \
     -v ./results_hisat2/:/results_hisat2/ \
     quay.io/biocontainers/gffcompare:0.12.6--h9948957_4 \
     bash -c " 
-        cp /results_hisat2/genome/hybrid_gencode.filtered.gtf hybrid_gencode.filtered.gtf && \
-        gffcompare -V -r hybrid_gencode.filtered.gtf \
+        cp /results_hisat2/genome/hybrid_gencode_fixed.filtered.gtf hybrid_gencode_fixed.filtered.gtf && \
+        gffcompare -V -r hybrid_gencode_fixed.filtered.gtf \
         -o /stringtie/compare \
         /stringtie/stringtie_merged_transcriptome.gtf 
     "
