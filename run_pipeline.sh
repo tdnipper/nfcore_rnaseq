91499e0c185ae2ef4b5fc9841ce3585beb76ae07
Thomas <nipper@wisc.edu>
2025-08-01 18:39:12 +0000

    fix: update run_pipeline.sh with correct input file and additional parameters

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -2,21 +2,21 @@
 
 nextflow run nf-core/rnaseq \
     -r 3.19.0 \
-    --input samplesheet.csv \
+    --input samplesheet_unsplit.csv \
     --fasta genomes/hybrid_gencode_reformat.fa.gz \
     --gtf genomes/hybrid_gencode_fixed.gtf.gz \
     --outdir results_hisat2 \
     --with_umi \
-    --umitools_bc_pattern NNNNNNNNNNN \
+    --umitools_bc_pattern NNNNNNNNNNNN \
     --aligner hisat2 \
     --skip_pseudo_alignment \
-    --salmon_quant_libtype ISR \
     -profile podman \
-    --skip_trimming \
     --featurecounts_group_type gene_type \
     --save_unaligned \
-    --multiqc_title gencode \
     --save_reference \
     --save_align_intermeds \
+    --remove_ribo_rna \
+    --trimmer fastp \
+    --extra_fastp_args "--adapter_sequence AGATCGGAAGAGCACACGTCTGAACTCCAGTCA --adapter_sequence_r2 AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT" \
     -resume \
     --gencode
