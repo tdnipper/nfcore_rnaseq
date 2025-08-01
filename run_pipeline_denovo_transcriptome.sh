a421630944d8bc0dd55af15e90fc3ce9a1106902
Thomas <nipper@wisc.edu>
2025-08-01 18:39:07 +0000

    fix: update run_pipeline_denovo_transcriptome.sh with correct input and GTF file names

--- run_pipeline_denovo_transcriptome.sh
+++ run_pipeline_denovo_transcriptome.sh
@@ -2,20 +2,21 @@
 
 nextflow run nf-core/rnaseq \
     -r 3.19.0 \
-    --input samplesheet.csv \
+    --input samplesheet_unsplit.csv \
     --fasta genomes/hybrid_gencode_reformat.fa.gz \
-    --gtf stringtie/stringtie_transcriptome.filtered.gtf.gz \
+    --gtf stringtie/stringtie_merged_transcriptome.filtered.gtf.gz \
     --outdir results_denovoTranscriptome \
     --with_umi \
-    --umitools_bc_pattern NNNNNNNNNNN \
-    --aligner star_salmon \
+    --umitools_bc_pattern NNNNNNNNNNNN \
+    --skip_alignment \
     --pseudo_aligner salmon \
     --salmon_quant_libtype ISR \
     -profile podman \
-    --skip_trimming \
     --save_unaligned \
-    --multiqc_title gencode \
     --save_reference \
+    --remove_ribo_rna \
+    --trimmer fastp \
+    --extra_fastp_args "--adapter_sequence AGATCGGAAGAGCACACGTCTGAACTCCAGTCA --adapter_sequence_r2 AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT" \
     -resume \
     --featurecounts_group_type gene_type \
     --gencode
