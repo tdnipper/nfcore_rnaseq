359f723cf70122410b2db0d8841a4e37e8d02971
Thomas <nipper@wisc.edu>
2025-12-03 00:02:18 +0000

    fix: update directory path resolution and ensure all scripts are executed in runner.sh

--- runner.sh
+++ runner.sh
@@ -2,7 +2,7 @@
 SAMPLESHEET=$1
 FASTA=$2
 GTF=$3
-DIR=$(dirname $0)
+DIR="$(cd "$(dirname "$0")" && pwd)"
 
 if [ ! -f "$SAMPLESHEET" ]; then
 	echo "Usage: bash run_pipeline.sh <path_to_samplesheet> <path_to_genome_fasta> <path_to_genome_gtf>"
@@ -21,4 +21,5 @@ fi
 
 bash scripts/run_pipeline.sh $SAMPLESHEET $FASTA $GTF $DIR && \
 bash scripts/stringtie_denovo.sh $GTF $DIR && \
-bash scripts/stringtie_merge.sh $GTF $DIR
+bash scripts/stringtie_merge.sh $GTF $DIR && \
+bash scripts/run_pipeline_denovo_transcriptome.sh $SAMPLESHEET $FASTA $GTF $DIR
