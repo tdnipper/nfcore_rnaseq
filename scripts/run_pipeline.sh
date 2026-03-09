7ff2cd1220fc7e1e8c9fde22b1601a8627e82773
Thomas <nipper@wisc.edu>
2026-03-09 22:10:52 +0000

    bump nextflow version to 3.23 in pipeline scripts

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -4,7 +4,7 @@ FASTA=$2
 GTF=$3
 DIR=$4
 nextflow run nf-core/rnaseq \
-    -r 3.22.0 \
+    -r 3.23 \
     --input $SAMPLESHEET \
     --fasta $FASTA \
     --gtf $GTF \
@@ -12,11 +12,12 @@ nextflow run nf-core/rnaseq \
     --with_umi \
     --umitools_bc_pattern NNNNNNNNNNNN \
     --aligner hisat2 \
+    --hisat2_index $DIR/hisat2_index.tar.gz \
+    --save_reference \
     --skip_pseudo_alignment \
     -profile podman \
     --save_unaligned \
-    --save_reference \
     --save_align_intermeds \
     --trimmer fastp \
-    --extra_fastp_args "--adapter_sequence AGATCGGAAGAGCACACGTCTGAACTCCAGTCA --adapter_sequence_r2 AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT" \
+    --extra_fastp_args "--adapter_sequence AGATCGGAAGAGCACACGTCTGAACTCCAGTCA --adapter_sequence_r2 AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT --trim_poly_g" \
     -resume
